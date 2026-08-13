af3285a42e9cc16b134a8769276fd7f4f0130efd
soheibayoub08soheib-eng <soheibayoub08soheib@gmail.com>
2026-08-13 19:17:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, File, Form
+from fastapi import FastAPI, UploadFile, File, From
 from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
 import librosa
@@ -7,44 +7,47 @@ import io
 app = FastAPI()
 
 app.add_middleware(
-CORSMiddleware,
-allow_origins=["*"],
-allow_credentials=True,
-allow_methods=["*"],
-allow_headers=["*"],
+  CORSMiddleware,
+  allow_origins=["*"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
 )
 
 @app.post("/analyze-audio")
 async def analyze_audio(
-audio: UploadFile = File(...),
-surah: str = Form(...),
-riwaya: str = Form(...)
+  audio: UploadFile = File(...),
+  surah: str = Form(...),
+  riwaya: str = Form(...)
 ):
-try:
-contents = await audio.read()
-audio_file = io.BytesIO(contents)
-y, sr = librosa.load(audio_file, sr=None)
-
-mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
-spectral_centroid = np.mean(librosa.feature.spectral_centroid(y=y, sr=sr))
-duration = librosa.get_duration(y=y, sr=sr)
-
-has_error = bool(spectral_centroid > 3500 or duration < 1.0)
-
-if has_error:
-analysis_result = {
-"status": "error",
-"message": "Error detected in Quran recitation rules or articulation points.",
-"details": {"spectral_centroid": float(spectral_centroid), "duration": float(duration)}
-}
+  try:
+    contents = await audio.read()
+    audio_file = io.BytesIO(contents)
+    y, sr = librosa.load(audio_file, sr=None)
+    
+    mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
+    spectral_centroid = np.mean(librosa.feature.spectral_centroid(y=y, sr=sr))
+    duration = librosa.get_duration(y=y, sr=sr)
+    
+    has_error = bool(spectral_centroid > 3500 or duration < 1.0)
+    
+    if has_error:
+      analysis_result = {
+        "status": "error",
+        "message": f"تنبيه في تلاوة سورة {surah} برواية {riwaya} تم رصد ملاحظة في احكام التلاوة.",
+        "details": {"spectral_centroid": float(spectral_centroid), "duration": float(duration)}
+      }
 else:
-analysis_result = {
-"status": "success",
-"message": "The recitation is accurate and matches the rules successfully.",
-"details": {"spectral_centroid": float(spectral_centroid), "duration": float(duration)}
-}
+      analysis_result = {
+        "status": "success",
+        "message": f"تلاوة سورة ما شاء الله {surah} برواية {riwaya} دقيقة ومطابقة للأحكام بنجاح.",
+        "details": {"spectral_centroid": float(spectral_centroid), "duration": float(duration)}
+      }
 
 return analysis_result
 
 except Exception as e:
 return {"status": "error", "message": f"حدث خطأ في المعالجة الفنية: {str(e)}"}
+
+
+
