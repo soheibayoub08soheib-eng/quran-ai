1abd0a7bc5af660ef385bdc5555b82c8345c4b51
soheibayoub08soheib-eng <soheibayoub08soheib@gmail.com>
2026-08-15 12:23:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ CORS(app)
 
 # إعداد مفتاح الـ API ورابط الاتصال المباشر
 api_key = os.environ.get("GOOGLE_API_KEY")
-url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={api_key}"
+url = f"https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key={api_key}"
 
 @app.route('/analyze-audio', methods=['POST'])
 def analyze_audio():
