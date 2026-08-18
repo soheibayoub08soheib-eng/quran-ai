9a27868689f62ff91fbd52bca5a46d11ad056427
soheibayoub08soheib-eng <soheibayoub08soheib@gmail.com>
2026-08-18 13:51:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def analyze_audio():
 """
 
         completion = groq_client.chat.completions.create(
-            model="llama3-8b-8192",
+            model="llama3-70b-8192",
             messages=[
                 {"role": "user", "content": prompt}
             ],
